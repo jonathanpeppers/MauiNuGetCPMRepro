27ec4187e2eba8f7029ed4550fb08d37f4f09d71
hypdeb <julien.debache@hotmail.com>
2023-01-27 12:54:40 +0100

    repro for NU1009 with MAUI projects

---
+++ Repro.Lib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Repro.Lib;
+public class Class1
+{
+  private readonly ILogger<Class1> logger;
+
+  public Class1(ILogger<Class1> logger) => this.logger = logger;
+}
